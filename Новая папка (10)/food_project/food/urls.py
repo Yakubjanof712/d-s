6c1f504dea4988788c30384c1be46a9b58a8dc32
Yakubjanof712 <husanovaguljahon919@gmail.com>
2025-02-12 05:42:24 +0300

    Delete Новая папка (10) directory

--- Новая папка (10)/food_project/food/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('foods/', views.FoodList.as_view(), name='food-list'),
-    path('foods/<int:pk>/', views.FoodDetail.as_view(), name='food-detail'),
-]
